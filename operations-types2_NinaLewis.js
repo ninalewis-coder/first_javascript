a3dd4e5c02a6e7f51ec74c7c6633e917415a6484
ninalewis-coder <ninarlewis99@gmail.com>
2025-10-30 14:14:44 +0200

    first commit

---
+++ operations-types2_NinaLewis.js
@@ -0,0 +1,21 @@
+// TODO: Create a variable called number1 and store any number in it
+let number1 = 5;
+
+// TODO: Create a variable called number2 and store a different number in it
+let number2 = 6;
+
+// TODO: Convert number1 to a string and store it in a new variable
+const number1String = String(number1);
+
+// TODO: Add number1 and number2 and store the result in a variable called sum
+const sum = number1 + number2;
+
+// TODO: Multiply number1 and number2 and store the result in a variable called product
+const product = number1 * number2;
+
+// TODO: Print all results to the console 
+console.log("Number 1: " + number1);
+console.log("Number 2: " + number2);
+console.log("Number 1 as a string: " + number1String);
+console.log("Sum: " + sum);
+console.log("Product: " + product);
