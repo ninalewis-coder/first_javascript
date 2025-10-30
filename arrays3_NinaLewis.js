e5cd5487a6464a28c8eeac1ca2fde3cbf6617b2f
ninalewis-coder <ninarlewis99@gmail.com>
2025-10-30 14:58:13 +0200

    added to arrays3

---
+++ arrays3_NinaLewis.js
@@ -0,0 +1,22 @@
+// TODO: Create an empty array called colors
+let colours = [];
+
+// TODO: Add three colors to the array using push()
+colours.push("green");
+colours.push("pink");
+colours.push("purple");
+
+// TODO: Create another array called numbers with five numbers
+let numbers = [10, 20, 30, 40, 50];
+
+// TODO: Remove the last color from the colors array
+colours.pop();
+
+// TODO: Add a new color to the beginning of the colors array
+colours.unshift("brown");
+
+// TODO: Print the length of both arrays
+console.log("Colours: " + colours);
+console.log("Length of colours array: " + colours.length);
+console.log("Numbers: " + numbers);
+console.log("Length of numbers array: " + numbers.length);
