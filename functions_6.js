bcb9f817ddb048b695fb5c20d00b34554be3b22c
ninalewis-coder <ninarlewis99@gmail.com>
2025-11-04 14:26:44 +0200

    first commit

---
+++ functions_6.js
@@ -0,0 +1,18 @@
+// TODO: Create a function called greet that takes a name parameter
+// TODO: Make the greet function return "Hello, [name]!"
+function greet(name){
+    return "Hello, " =  name = "!";
+}
+// TODO: Create a function called add that takes two parameters
+// TODO: Make the add function return the sum of both parameters
+function add(a,b){
+    return a + b;
+}
+// TODO: Call both functions and store their results in variables
+const greeting = greet("Nina");
+const sum = add(5,6);
+
+// TODO: Print the results to the console
+console.log(gretting);
+console.log(sum);
+
