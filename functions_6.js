089f98e993b8ad1218e5b6356d134138f2f3c5f8
ninalewis-coder <ninarlewis99@gmail.com>
2025-11-04 15:22:49 +0200

    rectified mistakes

--- functions_6.js
+++ functions_6.js
@@ -1,7 +1,7 @@
 // TODO: Create a function called greet that takes a name parameter
 // TODO: Make the greet function return "Hello, [name]!"
 function greet(name){
-    return "Hello, " =  name = "!";
+    return "Hello, " + name + "!";
 }
 // TODO: Create a function called add that takes two parameters
 // TODO: Make the add function return the sum of both parameters
@@ -10,9 +10,9 @@ function add(a,b){
 }
 // TODO: Call both functions and store their results in variables
 const greeting = greet("Nina");
-const sum = add(5,6);
+let sum = add(5,6);
 
 // TODO: Print the results to the console
-console.log(gretting);
+console.log(greeting);
 console.log(sum);
 
